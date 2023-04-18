e5372f00c748862425d8a2fa08bcaf676e8f1952
rafalos <rsulkowski1@gmail.com>
2023-04-18 12:34:08 +0200

    add user register form and request

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function RegisterPage() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  async function registerHandler(e) {
+    e.preventDefault();
+    await axios.post('/register', {
+      name,
+      email,
+      password,
+    });
+
+    alert('Registration succesfull, now you can log in');
+  }
+
   return (
     <div className='mt-4 grow flex items-center justify-center'>
       <div className='mb-64'>
         <h1 className='text-4xl text-center mb-4'>Register</h1>
-        <form className='max-w-lg mx-auto'>
-          <input type="text" placeholder='John Doe' />
-          <input type='email' placeholder='your@email.com' />
-          <input type='password' placeholder='password' />
-          <button className='primary'>Login</button>
+        <form className='max-w-lg mx-auto' onSubmit={registerHandler}>
+          <input
+            type='text'
+            placeholder='John Doe'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type='email'
+            placeholder='your@email.com'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type='password'
+            placeholder='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className='primary'>Register</button>
           <div className='text-center py-2 text-grey-500'>
             Already a member?{' '}
             <Link className='underline text-black' to='/login'>
